ab01ef1d6a7d7725063770a4faef734aec190f8f
R1sh11172 <rishi.aniga@gmail.com>
2023-09-22 14:37:15 -0400

    commit with basic game functionality

--- app/src/main/java/com/example/cs2340b_team39/MainActivity.java
+++ app/src/main/java/com/example/cs2340b_team39/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cs2340b_team39;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.RadioGroup;
@@ -7,22 +8,24 @@ import android.widget.RadioGroup;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startBtn = findViewById(R.id.startbutton);
+    private Button endBtn = findViewById(R.id.quitButton);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
-        Button startBtn = findViewById(R.id.startbutton);
-        Button endBtn = findViewById(R.id.quitButton);
-
         startBtn.setOnClickListener(v -> {
             //System.out.println("Button works");
-            setContentView(R.layout.activity_configuration);
+            openConfigScreen();
         });
 
         endBtn.setOnClickListener(v -> {
             System.exit(0);
         });
+    }
 
-
+    public void openConfigScreen() {
+        Intent intent = new Intent(this, ConfigActivity.class);
+        startActivity(intent);
     }
 }
