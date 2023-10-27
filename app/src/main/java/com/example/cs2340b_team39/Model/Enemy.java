a9b7ad8a1c63df2df4362cb31b92ce30e217259b
kraugelcodes <joshua.blackmon326@gmail.com>
2023-10-27 14:53:29 -0400

    Implemented basic enemy movement commands and methods

--- app/src/main/java/com/example/cs2340b_team39/Model/Enemy.java
+++ app/src/main/java/com/example/cs2340b_team39/Model/Enemy.java
@@ -5,4 +5,18 @@ public class Enemy {
     private double speed;
     private double damage;
     private double size;
+    private double x, y;
+    private EnemyMovement movement;
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
 }
---
+++ app/src/main/java/com/example/cs2340b_team39/Model/EnemyMovement.java
@@ -0,0 +1,110 @@
+package com.example.cs2340b_team39.Model;
+
+import android.widget.ImageView;
+
+import com.example.cs2340b_team39.View.GameActivity;
+import com.example.cs2340b_team39.View.GameActivityMap2;
+import com.example.cs2340b_team39.View.GameActivityMap3;
+import com.example.cs2340b_team39.ViewModel.ViewModel;
+
+public class EnemyMovement implements Movement {
+    public Enemy enemy;
+    public EnemyMovement(Enemy e) {
+        this.enemy = e;
+    }
+    public void moveLeft(int speed) {
+        enemy.setX(enemy.getX() - speed);
+        if (checkCollisions()) {
+            enemy.setX(enemy.getX() + speed);
+        }
+    }
+    public void moveRight(int speed) {
+        enemy.setX(enemy.getX() + speed);
+        if (checkCollisions()) {
+            enemy.setX(enemy.getX() - speed);
+        }
+    }
+    public void moveUp(int speed) {
+        enemy.setY(enemy.getY() - speed);
+        if (checkCollisions()) {
+            enemy.setY(enemy.getY() + speed);
+        }
+    }
+    public void moveDown(int speed) {
+        enemy.setY(enemy.getY() + speed);
+        if (checkCollisions()) {
+            enemy.setY(enemy.getY() - speed);
+        }
+    }
+    public boolean checkCollisions() {
+        //if (testCase) {
+        //    return false;
+        //}
+        int map = ViewModel.checkMap();
+        if (map == 1) {
+            if (enemy.getX() <= 5) {
+                return true;
+            }
+            if (enemy.getX() >= GameActivity.getWidth() - 25) {
+                return true;
+            }
+            ImageView top = GameActivity.getTop();
+            ImageView leftC = GameActivity.getLeftC();
+            ImageView rightC = GameActivity.getRightC();
+            ImageView bottom = GameActivity.getBottom();
+            if (enemy.getY() <= top.getY() + 40) {
+                return true;
+            }
+            if (enemy.getY() <= leftC.getY() + 40
+                    && enemy.getX() <= leftC.getX() + 25) {
+                return true;
+            }
+            if (enemy.getY() <= rightC.getY() + 40
+                    && enemy.getX() >= rightC.getX() + 25) {
+                return true;
+            }
+            if (enemy.getY() >= bottom.getY() + 40) {
+                return true;
+            }
+        }
+        if (map == 2) {
+            ImageView top = GameActivityMap2.getTop();
+            ImageView bottom = GameActivityMap2.getBottom();
+            if (enemy.getX() <= 5) {
+                return true;
+            }
+            if (enemy.getX() >= GameActivity.getWidth() - 25) {
+                return true;
+            }
+            if (enemy.getY() >= bottom.getY() + 40) {
+                return true;
+            }
+            if (enemy.getY() <= top.getY() + 40) {
+                return true;
+            }
+        }
+        if (map == 3) {
+            ImageView top = GameActivityMap3.getTop();
+            ImageView bottom = GameActivityMap3.getBottom();
+            ImageView leftC = GameActivityMap3.getLeftC();
+            if (enemy.getX() <= 5) {
+                return true;
+            }
+            if (enemy.getX() >= GameActivity.getWidth() - 25) {
+                return true;
+            }
+            if (enemy.getY() >= bottom.getY() + 40) {
+                return true;
+            }
+            if (enemy.getY() <= top.getY() + 40) {
+                return true;
+            }
+            if (enemy.getY() <= leftC.getY() + 40
+                    && enemy.getX() <= leftC.getX() + 25) {
+                return true;
+            }
+        }
+        CollisionSub.getCollision();
+        return false;
+    }
+}
