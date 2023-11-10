d1b92e10310b530bf08bb58a689a4a1d01c43240
divyagramaswamy <122933871+divyagramaswamy@users.noreply.github.com>
2023-11-10 14:45:50 -0500

    made versions of enemy

---
+++ app/src/main/java/com/example/cs2340b_team39/Model/Enemy2.java
@@ -0,0 +1,36 @@
+package com.example.cs2340b_team39.Model;
+
+public class Enemy2 extends Enemy {
+    private double health;
+    private double speed;
+    private double damage;
+    private double size;
+    private double x;
+    private double y;
+    private EnemyMovement movement;
+
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public double getDamage() {
+        return damage;
+    }
+    public double getSize() {
+        return size;
+    }
+    public void doubleSize() {
+        size *= 2;
+    }
+
+
+
+}
