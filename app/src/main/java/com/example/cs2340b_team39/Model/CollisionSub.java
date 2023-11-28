5453b4a7b9dd51352aecf365c8666f54f9c132a4
kraugelcodes <joshua.blackmon326@gmail.com>
2023-11-27 22:28:12 -0500

    Added weapon movement and score updates

--- app/src/main/java/com/example/cs2340b_team39/Model/CollisionSub.java
+++ app/src/main/java/com/example/cs2340b_team39/Model/CollisionSub.java
@@ -58,6 +58,9 @@ public class CollisionSub {
                 ViewModel.updateHealth();
             } else if (collide && player.getIsAttacking()) {
                 enemies.get(i).setIsDead(true);
+                Player.getPlayer().getScore().setScore(Player.getPlayer().getScore().getScore() + 50);
+                ViewModel.updateScore(Player.getPlayer().getScore().getTextView(),
+                        Player.getPlayer().getScore().getScore());
             }
         }
     }
